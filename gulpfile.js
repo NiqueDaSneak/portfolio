116242aa1712a49878abd45e93e42a11178d50d1
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-19 12:36:41 -0400

    add html2pug dependency to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const gutil = require('gulp-util');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const gutil = require('gulp-util')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const html2pug = require('gulp-html2pug')
+
 
 gulp.task('sass', function(){
   return gulp.src('./public/stylesheets/sass/*.sass')
@@ -12,10 +14,17 @@ gulp.task('sass', function(){
         .pipe(postcss([ autoprefixer() ]))
         .on('error', gutil.log)
         .pipe(gulp.dest('./public/stylesheets'))
+})
+
+gulp.task('pug', function() {
+  return gulp.src('views/index.html')
+  .pipe(html2pug())
+  .pipe(gulp.dest('pug'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('public/stylesheets/home.sass', ['sass']);
-});
+gulp.watch('views/index.html', ['pug'])
+  gulp.watch('public/stylesheets/home.sass', ['sass'])
+})
 
-gulp.task('default', ['watch', 'sass']);
+gulp.task('default', ['watch', 'sass', 'pug'])
