f4b7583a55a46bdd9eef472c52ef3abb0be55477
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-20 16:00:55 -0400

    restyle menu and finish animation to slide in menu

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,15 @@ $(document).ready(() => {
 
     // UI
     $('.main-content').fadeIn(1200)
-    $('.links:last-child').on('click', () => {
-      $('.menu').css('bottom', '2vh')
-      return false
+
+    $('.toggle').on('click', () => {
+        if ($('.toggle').hasClass('open')) {
+            $('.menu').css('bottom', '-5vh')
+            $('.toggle').removeClass('open').text('MORE')
+        } else {
+            $('.menu').css('bottom', '2vh')
+            $('.toggle').addClass('open').text('CLOSE')
+        }
+        return false
     })
 })
