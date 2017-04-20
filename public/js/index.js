39738c8fd3ad9de85c9acfd27b2a60ae07db6d3f
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-20 13:16:52 -0400

    add jquery to slide menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(() => {
 
+    // SOCKET & BOT CLIENT
     var socket = io.connect();
 
+    // UI
     $('.main-content').fadeIn(1200)
+    $('.links:last-child').on('click', () => {
+      $('.menu').css('bottom', '2vh')
+      return false
+    })
 })
