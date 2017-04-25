62746804fbc7f876fda4714b8f5cb9ec5481caf3
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-25 13:07:13 -0400

    start logic for client sending bot data to server

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,23 @@ $(document).ready(() => {
 
     // SOCKET & BOT CLIENT
     var socket = io.connect();
+    socket.on('botMessage', (data) => {
+        console.log(data.data)
+        $('.messages').prepend("<p class='bot-message'>" + data.data + "</p>")
+    })
+    socket.on('menuButtons', (data) => {
+        for (var i = 0; i < data.data.length; i++) {
+            $('.btns').append("<button type='button' value='" + data.data[i] + "'>" + data.data[i] + "</button>")
+            $('.btns').addClass('exposed')
+        }
+    })
 
     // UI
+
+    // INITIAL FADE IN
     $('.main-content').fadeIn(1200)
 
+    // MENU TOGGLE
     $('.toggle').on('click', () => {
         if ($('.toggle').hasClass('open')) {
             $('.menu').css('bottom', '-5vh')
@@ -16,4 +29,29 @@ $(document).ready(() => {
         }
         return false
     })
+
+    // CHAT
+
+    // load welcomeMessage
+    $('.chat').on('click', () => {
+        $('.main-content').fadeOut(1200).promise().done(() => {
+            $('.chat-ui').fadeIn(1200).promise().done(() => {
+                socket.emit('welcomeMessage')
+            })
+        })
+    })
+
+    $('.chat-ui header a').click(() => {
+        $('.chat-ui').fadeOut(1200, () => {
+            $('.main-content').fadeIn(1200)
+        })
+        return false
+    })
+
+    $('.btns').click(() => {
+      socket.emit('menuRequest', { data: event.target.value })
+      $('.btns').removeClass('exposed')
+    })
+
+    // end of DOM READY
 })
