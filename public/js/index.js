c41dccfd85018e937eaa42d247c24454ac873b4f
Dom Clemmer <thisisdom4@gmail.com>
2017-05-10 12:07:34 -0400

    add orientation feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(() => {
 
+
+  $(window).on("orientationchange", (event) => {
+
+    if (window.orientation === 90) {
+      $('.hide-orientation').fadeIn()
+    }
+
+    if (window.orientation === -90) {
+      $('.hide-orientation').fadeIn()
+    }
+
+    if (window.orientation === 0) {
+        $('.hide-orientation').fadeOut()
+    }
+  })
+
     // SOCKET & BOT CLIENT
     var socket = io.connect();
     socket.on('botMessage', (data) => {
